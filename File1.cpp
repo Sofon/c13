4af02053dbd7972d57e93e1001bb5de19955c2d9
Sofon <Sofon000@ya.ru>
2013-06-04 18:43:57 +0400

    6

--- File1.cpp
+++ File1.cpp
@@ -7,32 +7,29 @@ void printArray ( int [] ); int main() {
 int n,i,k;
 float *a;
 float *c;
-float s,p;
+float s,p,h;
 
 printf("n=? ");
 scanf("%d", &n);
 if (n<1 || n>200) {printf("Invalid n! \nPress any key");getch();}
 
 a = new float [n];
-c = new float [n];
+
 
 printf("Input a(%d): \n", n);
 for (i = 0; i < n; i++) {scanf("%f", &a[i]);}
-printf("Input c(%d): \n", n);
-for (i = 0; i < n; i++) {scanf("%f", &c[i]);}
+
 printf("a(%d): \n", n);
 for (i = 0; i < n; i++) { printf("%4.1f ", a[i]);}
 printf("\n");
-printf("c(%d): \n", n);
-for (i = 0; i < n; i++) { printf("%4.1f ", c[i]);}
-printf("\n");
-s=a[0]*c[0]; p=a[0]*c[0];
-for (i = 1; i < n; i++) {s=a[i]*c[i]; if (s > p){p=s;}
+
+s=a[0]*a[0]; p=a[0]*a[0];
+for (i = 1; i < n; i++) {s=a[i]*a[i]; if (s > p){p=s;
+h=i+1;}
 }
 delete [] a;
-delete [] c;
 printf("Max=");
-printf("%4.1f", p);
+printf("%4.0f", h);
 printf("\n");
 printf("Press any key") ;
 getch(); // ожидание нажатия клавиши
