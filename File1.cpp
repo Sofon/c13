4ad55c5c39e06e35be411754a96304442785b883
Sofon <Sofon000@ya.ru>
2013-06-06 08:51:13 +0400

    Update File1.cpp

--- File1.cpp
+++ File1.cpp
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 ﻿#include <iostream.h>
-=======
+
 #include <iostream.h>
->>>>>>> 66e9ad60fea109c7fc850230c132180c0a6ad899
 #include <conio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -12,34 +11,34 @@ int n,i,k;
 float *a;//одномерный массив
 
 float s,p;//перемены для нахождение промежуточного максимума
-=======
-float *a;
+
+
 
 float s,p,h;
->>>>>>> 66e9ad60fea109c7fc850230c132180c0a6ad899
+
 
 printf("n=? ");
 scanf("%d", &n);//вводим размерность радя
 if (n<1 || n>200) {printf("Invalid n! \nPress any key");getch();}
 
-<<<<<<< HEAD
+
 a = new float [n];//создаем массив их n элементов
 
 
 printf("Input a(%d): \n", n);
 for (i = 0; i < n; i++) {scanf("%f", &a[i]);}//читаем элементы массива
-=======
+
 a = new float [n];
 
 
 printf("Input a(%d): \n", n);
 for (i = 0; i < n; i++) {scanf("%f", &a[i]);}
->>>>>>> 66e9ad60fea109c7fc850230c132180c0a6ad899
+
 
 printf("a(%d): \n", n);
 for (i = 0; i < n; i++) { printf("%4.1f ", a[i]);}
 printf("\n");
-<<<<<<< HEAD
+
 float h=1;
 // избагаем аномалии если поставить два чесал(равных по модулю)
 //например -4 и 4 в начало массива а остольные a[i]<4 то
@@ -56,7 +55,7 @@ printf("%4.0f", h);
 printf("\n");
 printf("Press any key") ;
 getch(); // Ожидание нажатие клавиши enter для завершение выполение программы
-=======
+
 
 s=a[0]*a[0]; p=a[0]*a[0];
 for (i = 1; i < n; i++) {s=a[i]*a[i]; if (s > p){p=s;
@@ -68,7 +67,7 @@ printf("%4.0f", h);
 printf("\n");
 printf("Press any key") ;
 getch(); // �������� ������� �������
->>>>>>> 66e9ad60fea109c7fc850230c132180c0a6ad899
+
 
 
 }
